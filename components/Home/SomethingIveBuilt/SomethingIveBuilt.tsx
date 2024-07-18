2a1b52a332f4adf3cdb4fffcf26273f9ce604cbd
Hasan Tariq <hkhankor@gmail.com>
2024-07-18 23:04:32 +0500

    first commit

---
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -0,0 +1,379 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import ArrowIcon from "../../Icons/ArrowIcon";
+import Img from "../../smallComp/image/Img";
+import GithubIcon from "../../Icons/GithubIconForSomethingIveBuild";
+import ExternalLink from "../../Icons/ExternalLink";
+
+export default function SomethingIveBuilt() {
+  const router = useRouter();
+  return (
+    <div
+      id="SomethingIveBuiltSection"
+      className=" flex flex-col xl:space-y-28 space-y-12 bg-AAprimary w-full
+     2xl:px-72 lg:px-24 md:px-16 sm:px-16 py-32 px-4"
+    >
+      {/* // ? Title  */}
+      <div data-aos="fade-up" className=" flex flex-row  items-center md:px-0">
+        <ArrowIcon className={"flex-none h-5 md:h-6 w-5 md:w-5 translate-y-[2px] text-AAsecondary"} />
+        <div className="flex-none flex-row space-x-2 items-center pr-2">
+          <span className="text-AAsecondary font-sans text-sm  sm:text-xl"> 03.</span>
+          <span className=" font-bold tracking-wider text-gray-200 text-lg md:text-2xl w-44 md:w-56 opacity-85">
+            {" "}
+            Some Things I&apos;ve Built
+          </span>
+        </div>
+        <div className="bg-gray-400 h-[0.2px] w-full xl:w-1/3 md:w-1/2"></div>
+      </div>
+
+      <div className="flex flex-col   xl:space-y-36 space-y-8 md:space-y-28">
+        {/* // ?  Project  1*/}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              {/* <Link href={"/typing"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary
+           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link> */}
+
+              <a href="https://shadihall2.vercel.app/" target={"_blank"} rel="noreferrer">
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAprimary
+           transition-opacity opacity-30 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+              <img src="https://ik.imagekit.io/os33grffu/Sh1.PNG?updatedAt=1721150291332" alt="Project Screen shot" className="w-full h-full" />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/ensvision.jpg"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">ShadiHall.com </span>
+                <a href="https://ens.vision" target="_blank" rel="noopener noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                   Wedding Halls Booking
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+              <p className="text-gray-300 md:text-gray-400 text-left md:text-right text-lg md:text-xl">
+  I have worked as a full stack dev to complete this wedding halls booking app, including its login and signup, list of halls, halls booking, and chatting features.
+</p>
+
+
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Socket.IO Chat </span>
+                <span className="pr-4 z-10">NextJS</span>
+                <span className="pr-4 z-10">MongoDB</span>
+                <span className="pr-4 z-10">Auth</span>
+                <span className="pr-4 z-10">ERC72</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <a href="https://shadihall2.vercel.app/" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* // ?  Project 2 */}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
+              <a href={"https://pakistaneyes.com/"} target="_blank" rel="noreferrer">
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary
+           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+              <Img src={"https://ik.imagekit.io/os33grffu/pe1.PNG?updatedAt=1721151396528"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/hackme.jpg"} alt={"Project Screen shot"} className={`w-full h-full`} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6
+            col-span-8 flex flex-col items-start  space-y-3 md:order-1"
+            >
+              <div className="flex flex-col space-y-1  z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <a href="https://pakistaneyes.com/" target="_blank" rel="noopener noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                   Pakiatan Eyes ( Dynamic News Website )
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+              <p className="text-gray-300 md:text-gray-400 text-left text-lg md:text-xl font-bold">
+  It's a  website where the admin can post news from the admin panel, and it will be visible to users.
+</p>
+
+
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-start"
+              >
+                <span className="pr-4 z-10">Token</span>
+                <span className="pr-4 z-10">ERC20</span>
+                <span className="pr-4 z-10">Nextjs</span>
+                <span className="pr-4 z-10">Smart contract</span>
+                <span className="pr-4 z-10">Blockchain</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+
+                <a href="https://pakistaneyes.com/" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* // ?  Project  3*/}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <Link href={"https://github.com/Hasankhankor/All-video-downloader"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link>
+
+              <Img src={"https://ik.imagekit.io/os33grffu/WhatsApp%20Image%202024-02-02%20at%205.56.22%20PM.jpeg?updatedAt=1721237791394"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/typing.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <Link href={"/typing"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Speed Typing
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                  This project aim to help you to improve your <span className="text-AAsecondary"> typing </span> by
+                  tracking your progress in <span className="text-AAsecondary"> each round</span> and give you a{" "}
+                  <span className="text-AAsecondary"> score</span> based on your typing speed and accuracy through a
+                  table of <span className="text-AAsecondary"> statistics</span>.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Algorithms</span>
+                <span className="pr-4 z-10">Framer Motion</span>
+                <span className="pr-4 z-10">Tailwind CSS</span>
+                <span className="pr-4 z-10">TypeScript</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/Hasankhankor/All-video-downloader" />
+                <ExternalLink url={"https://github.com/Hasankhankor/All-video-downloader"} router={router} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* // ?  Project 4 */}
+
+
+        {/* // ? Project 5 */}
+
+
+        {/* // ?  Project 6 */}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
+              <a href="https://greatfuture.org.pk/" target={"_blank"} rel="noreferrer">
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary
+           transition-opacity opacity-60 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img src={"https://ik.imagekit.io/os33grffu/great%20future%20pakistan.PNG?updatedAt=1710869437774"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAprimary opacity-50 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/CallCenter.png"} alt={"Project Screen shot"} className={`w-full h-full`} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6
+            col-span-8 flex flex-col items-start  sm:space-y-3 md:order-1"
+            >
+              <div className="flex flex-col space-y-1  z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <a href="https://greatfuture.org.pk/" target={"_blank"} rel="noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                   Great Future Pakistan (LMS)
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left ">
+                Great Future Talent Olympiad is an Online MCQs-based contest consisting of English, IQ, and General Knowledge to enhance the skills of students to compete in different practical tests like PMA, CSS, and Entry Tests
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-start"
+              >
+                <span className="pr-4 z-10">Wordpress</span>
+                <span className="pr-4 z-10">LMS</span>
+                <span className="pr-4 z-10">MultiThreading</span>
+                <span className="pr-4 z-10">MySQL</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+                <GithubIcon link="https://greatfuture.org.pk/" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <Link href={"/typing"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link>
+
+              <Img src={"https://ik.imagekit.io/os33grffu/dev1.PNG?updatedAt=1721152008469"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/typing.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <Link href={"/typing"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Hasan Dev Suite
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                  This project aim to help you the Tools Like <span className="text-AAsecondary"> Color, Image Converter   </span> by
+                 by getting color details and image details  <span className="text-AAsecondary"> QR code and Password Generator</span> {" "}Designed to streamline your workflow and enhance productivity, HassanTools offers everything you need in one convenient place
+                <span className="text-AAsecondary"> score</span> based on your typing speed and accuracy through
+                   <span className="text-AAsecondary">word counter, and keyboard tester.</span>.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Algorithms</span>
+                <span className="pr-4 z-10">Framer Motion</span>
+                <span className="pr-4 z-10">Tailwind CSS</span>
+                <span className="pr-4 z-10">TypeScript</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/Hasankhankor/HasanDevSuite" />
+                <ExternalLink url={"https://hasan-dev-suite.vercel.app/"} router={router} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
